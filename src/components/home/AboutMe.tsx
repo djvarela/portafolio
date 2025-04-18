5aee83d0a2495bbe7a592f8ca3555a41842a42d2
Diego <djvarela89@gmail.com>
2025-04-18 00:35:51 -0300

    about me terminado

--- src/components/home/AboutMe.tsx
+++ src/components/home/AboutMe.tsx
@@ -1,35 +1,49 @@
 export const AboutMe = () => {
   return (
-    <section>
+    <section className="aboutMe">
       <h2>Sobre Mí</h2>
       <h3>
         Un desarrollador full-stack apasionado, especializado en SEO y
         soluciones empresariales
       </h3>
 
-      <div className="aboutMe-info">
-        <h4>Trayectoria Profesional</h4>
-        <p>
-          Con más de 8 años de experiencia en desarrollo full-stack, me he
-          enfocado en crear aplicaciones empresariales robustas con énfasis en
-          SEO, sistemas CRM y soluciones ERP para la gestión de recursos
-          humanos. Mi pasión está en construir software escalable que impulse el
-          crecimiento empresarial y mejore la eficiencia operativa. Combino
-          experiencia técnica con conocimiento comercial para ofrecer soluciones
-          que realmente generen impacto
-        </p>
-        <span>
-            <a href="">Contactame</a>
-           
-        </span>
+      <div className="aboutMe-contentainer">
+
+
+        <div className="aboutMe-info">
+          <h4>Trayectoria Profesional</h4>
+          <p>
+            Con más de 8 años de experiencia en desarrollo full-stack, me he
+            enfocado en crear aplicaciones empresariales robustas con énfasis en
+            SEO, sistemas CRM y soluciones ERP para la gestión de recursos humanos</p>
+          <p>
+            Mi pasión está en construir software escalable que impulse el
+            crecimiento empresarial y mejore la eficiencia operativa. Combino
+            experiencia técnica con conocimiento comercial para ofrecer soluciones
+            que realmente generen impacto
+          </p>
+
+          <a href="#contacto" className="button-primary">Contactame</a>
+
+        </div>
+        <div className="experience">
+
+          <ul>
+            <li>
+              <div className="icon-experience" style={{ background: "" }}>
+                <img src="/images/soft.svg" alt="icon" width="30" />
+              </div>
+              <div className="info-experience">
+                <h2>Desarrollador Lead Full-Stack</h2>
+                <h3>Enterprise Solutions Inc. (2020 - Presente)</h3>
+                <p>Liderando el desarrollo de aplicaciones empresariales optimizadas para SEO</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+
       </div>
-      <div className="experience">
-        <ul>
-            <li>Experiencia 1 </li>
-            <li>Experiencia 2</li>
-            <li>Experiencia 3</li>
-        </ul>
-      </div>
+
     </section>
   );
 };
