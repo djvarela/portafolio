42858451fd98ce0f829f7c22d7914b3471c88205
Diego <djvarela89@gmail.com>
2025-04-27 21:14:57 -0300

    estructura de Footer

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -2,6 +2,31 @@
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+
+
+      <div className="info">
+        <h2>Diego Varela</h2>
+        <p>Desarrollo full-stack con enfoque en optimización SEO y soluciones empresariales.</p>
+
+        <ul>
+          <li>Githutb</li>
+          <li>Linkedin</li>
+          <li>Email</li>
+        </ul>
+      </div>
+
+      <div className="copy">
+        <h2>© 2025 DevFolio. Todos los derechos reservados.</h2>
+        <h3>Diseñado y desarrollado con pasión y atención a las mejores prácticas de SEO.</h3>
+      </div>
+
+
+
+
+
+
+
+    </footer>
   )
 }
