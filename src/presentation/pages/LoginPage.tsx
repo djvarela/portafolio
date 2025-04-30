44a6f322b5cc72c28f405f1aced30dca47e42afc
Diego <djvarela89@gmail.com>
2025-04-29 23:16:42 -0300

    Estilos de LoginPage terminada

--- src/presentation/pages/LoginPage.tsx
+++ src/presentation/pages/LoginPage.tsx
@@ -1,10 +1,35 @@
+import "../../../public/assets/css/normalize.css"
 
+import "../../../public/assets/css/styles.css"
 
 export const LoginPage = () => {
   return (
     <>
-    <title>Login</title>
-    <h2>Login</h2>
+      <title>Login</title>
+
+
+      <section className="loginPage">
+
+
+
+        <form action="" autoComplete="off">
+          <h2>Portafolio <br/> Iniciar sesion</h2>
+          <span>
+            <label htmlFor="Email">Email:</label>
+            <input type="text" name="email" />
+          </span>
+          <span>
+            <label htmlFor="password">Password:</label>
+            <input type="text" name="password" />
+          </span>
+
+          <button type="submit">INGRESAR</button>
+
+        </form>
+
+      </section>
+
+
     </>
   )
 }
